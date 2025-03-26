871332149eeabb2c40baaf81d0ddc06fe229c39d
richie-rich90454 <zhiyu.jiang90454-bisz@basischina.com>
2025-03-26 16:20:05 +0800

    fix: database.js

--- database.js
+++ database.js
@@ -1,8 +1,24 @@
-const mysql = require('mysql2')
-module.exports = mysql.createConnection({
+const mysql=require('mysql2/promise')
+module.exports=mysql.createConnection({
     host: '175.178.17.67',
     port: 3306,
     user: '175_178_17_67',
     password: 'Basis2023',
     database: '175_178_17_67',
+    waitForConnections: true,
+    connectionLimit: 10,
+    connectTimeout: 60000,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
+pool.getConnection()
+    .then(conn=>{
+        console.log('Connected to database!');
+        conn.release();
+    })
+    .catch(err=>{
+        console.error('Database connection failed:', err);
+        process.exit(1);
+    });
+module.exports=pool;
