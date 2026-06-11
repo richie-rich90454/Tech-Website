e886e036ec9e937678c2764dc1a0bad6f1499dff
richie-rich90454 <rjiang880@gmail.com>
2026-06-11 15:02:43 +0800

    add dev console greeting to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useEffect } from "react";
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import BasicInfo from "@/components/BasicInfo";
@@ -5,6 +8,11 @@ import TLCardGrid from "@/components/TLCardGrid";
 import Footer from "@/components/Footer";
 
 export default function Home() {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'development') {
+      console.log('👋 You found me! richie-rich90454 rebuilt this in June 2026. Happy exploring!');
+    }
+  }, []);
   return (
     <>
       <Navbar />
